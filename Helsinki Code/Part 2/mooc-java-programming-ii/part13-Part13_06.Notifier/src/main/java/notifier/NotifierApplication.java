2c3ccae7f25f8cb2f5a145255d40a66ccad48c05
roniantonius <roniantoniuss@upi.edu>
2025-02-26 22:01:43 +0700

    Part 13 about Event Handling. Learned how to use event handler interface, Change Listener, implement lambda on a user interface component to do some statistics.

--- Helsinki Code/Part 2/mooc-java-programming-ii/part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
+++ Helsinki Code/Part 2/mooc-java-programming-ii/part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
@@ -1,11 +1,35 @@
 package notifier;
 
-
-public class NotifierApplication {
-
-
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+public class NotifierApplication extends Application {
+    @Override
+    public void start(Stage window){
+        window.setTitle("Notifier App");
+        VBox layout = new VBox();
+        layout.setSpacing(10);
+        TextField atas = new TextField();
+        Button tombol = new Button("Update");
+        Label hasil = new Label();
+        tombol.setOnAction((fungsi) -> {
+            hasil.setText(atas.getText());
+        });
+        
+        layout.getChildren().addAll(atas, tombol, hasil);
+        
+        Scene scen = new Scene(layout);
+        window.setScene(scen);
+        window.show();
+        
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(NotifierApplication.class);
     }
 
 }
--- Helsinki Code/Part 2/mooc-java-programming-ii/part13-Part13_07.TextStatisticsPart2/src/main/java/textstatistics/TextStatisticsApplication.java
+++ Helsinki Code/Part 2/mooc-java-programming-ii/part13-Part13_07.TextStatisticsPart2/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,11 +1,57 @@
 package textstatistics;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
-public class TextStatisticsApplication {
-
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Arrays;
+public class TextStatisticsApplication extends Application{
+    @Override
+    public void start(Stage window){
+        window.setTitle("Statistics App");
+        
+        BorderPane layout = new BorderPane();
+        TextArea teks = new TextArea("");
+        layout.setCenter(teks);
+        
+        Label labelKarakter = new Label("Letters: 0");
+        Label labelJumlahKata = new Label("Words: 0");
+        Label labelKataPanjang = new Label("The longest word is: ");
+        
+        teks.textProperty().addListener((ubah, lama, baru) -> {
+            int karakterSekarang = baru.length();
+            String[] kata = baru.split(" ");
+            int jmlKata = kata.length;
+            String kataPanjangs = Arrays.stream(kata)
+                    .sorted((sebelum, sesudah) -> sesudah.length() - sebelum.length())
+                    .findFirst()
+                    .get();
+            
+            labelKarakter.setText("Letters: " + karakterSekarang);
+            labelJumlahKata.setText("Words: " + jmlKata);
+            labelKataPanjang.setText("The longest word is: " + kataPanjangs);
+        });
+        
+        HBox horijontal = new HBox();
+        horijontal.setSpacing(5);
+        
+        horijontal.getChildren().add(labelKarakter);
+        horijontal.getChildren().add(labelJumlahKata);
+        horijontal.getChildren().add(labelKataPanjang);
+        
+        layout.setBottom(horijontal);
+        
+        Scene sken = new Scene(layout);
+        window.setScene(sken);
+        window.show();
     }
-
+    public static void main(String[] args) {
+        launch(TextStatisticsApplication.class);
+    }
 }
