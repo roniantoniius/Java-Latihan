ec21e71ec58380e011a674f85b7caf9ec95a563a
roniantonius <roniantoniuss@upi.edu>
2025-02-27 22:39:04 +0700

    Part 13 about Multiple Views until now i learn creating layout for each view.

--- Helsinki Code/Part 2/mooc-java-programming-ii/part13-Part13_09.MultipleViews/src/main/java/application/MultipleViews.java
+++ Helsinki Code/Part 2/mooc-java-programming-ii/part13-Part13_09.MultipleViews/src/main/java/application/MultipleViews.java
@@ -1,9 +1,58 @@
 package application;
 
-public class MultipleViews {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class MultipleViews extends Application {
+    @Override
+    public void start(Stage window) {
+        Button back = new Button("To the second view!");
+        Button forth = new Button("To the third view!");
+        Button pertama = new Button("To the first view!");
+
+        BorderPane layoutPertama = new BorderPane();
+        layoutPertama.setTop(new Label("First view!"));
+        layoutPertama.setCenter(back); // Menambahkan button ke tengah
+        layoutPertama.setBottom(new Label("")); // Menambahkan label kosong di bawah jika diperlukan
+
+        VBox layoutKedua = new VBox();
+        layoutKedua.setSpacing(5);
+        layoutKedua.getChildren().add(forth);
+        layoutKedua.getChildren().add(new Label("Second view!"));
+
+        GridPane layoutKetiga = new GridPane();
+        layoutKetiga.add(new Label("Third view!"), 0, 0);
+        layoutKetiga.add(pertama, 1, 1);
+
+        Scene first = new Scene(layoutPertama);
+        Scene second = new Scene(layoutKedua);
+        Scene third = new Scene(layoutKetiga);
+
+        // ini tombol di halaman awal
+        back.setOnAction((event) -> {
+            window.setScene(second);
+        });
+
+        // ini tombol di halaman setelah
+        forth.setOnAction((event) -> {
+            window.setScene(third);
+        });
+
+        pertama.setOnAction((event) -> {
+            window.setScene(first);
+        });
+
+        window.setScene(first);
+        window.show();
     }
 
+    public static void main(String[] args) {
+        launch(MultipleViews.class);
+    }
 }
--- Helsinki Code/Part 2/mooc-java-programming-ii/part13-Part13_10.Greeter/src/main/java/application/GreeterApplication.java
+++ Helsinki Code/Part 2/mooc-java-programming-ii/part13-Part13_10.Greeter/src/main/java/application/GreeterApplication.java
@@ -1,9 +1,60 @@
 package application;
 
-public class GreeterApplication {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public class GreeterApplication extends Application {
+    @Override
+    public void start(Stage window){
+        // 1. Halaman Pertama
+        Button tombol1 = new Button("Start");
+        Label label1 = new Label("Enter your name to start.");
+        TextField pass1 = new TextField();
+        Label label2 = new Label("");
+        
+        GridPane layout1 = new GridPane();
+        layout1.add(label1, 0,0);
+        layout1.add(pass1, 0,1);
+        layout1.add(tombol1, 0,2);
+        layout1.add(label2, 0,3);
+        
+        layout1.setPrefSize(300, 180);
+        layout1.setAlignment(Pos.CENTER);
+        layout1.setVgap(10);
+        layout1.setHgap(10);
+        layout1.setPadding(new Insets(20, 20, 20, 20));
+        
+        Scene sken1 = new Scene(layout1);
+        
+        
+        // 2. Halaman setelah
+        Label label3 = new Label("Welcome ");
+        
+        StackPane layout2 = new StackPane();
+        layout2.setPrefSize(300, 180);
+        layout2.getChildren().add(label3);
+        layout2.setAlignment(Pos.CENTER);
+        
+        Scene sken2 = new Scene(layout2);
+        
+        tombol1.setOnAction((value) -> {
+            label3.setText(label3.getText() + pass1.getText() + "!");
+            window.setScene(sken2);
+        });
+        
+        window.setScene(sken1);
+        window.show();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hellow world! :3");
+        launch(GreeterApplication.class);
     }
 }
