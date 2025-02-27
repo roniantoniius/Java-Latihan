8f35a69f6c717b1650434d0040fc5a36f11d374d
roniantonius <roniantoniuss@upi.edu>
2025-02-27 19:14:18 +0700

    Part 13 about Applications lauch parameter like using Parameters class for create a input to title

--- Helsinki Code/Part 2/mooc-java-programming-ii/part13-Part13_08.UserTitle/src/main/java/title/Main.java
+++ Helsinki Code/Part 2/mooc-java-programming-ii/part13-Part13_08.UserTitle/src/main/java/title/Main.java
@@ -1,11 +1,16 @@
 package title;
 
-
+import javafx.application.Application;
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Masukkan judul program GUI");
+        String judul = scanner.nextLine();
+        Application.launch(UserTitle.class,
+                "--title=" + judul);
+        
     }
 
 }
--- Helsinki Code/Part 2/mooc-java-programming-ii/part13-Part13_08.UserTitle/src/main/java/title/UserTitle.java
+++ Helsinki Code/Part 2/mooc-java-programming-ii/part13-Part13_08.UserTitle/src/main/java/title/UserTitle.java
@@ -1,7 +1,23 @@
 package title;
 
+import javafx.application.Application;
+import javafx.application.Application.Parameters;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
-public class UserTitle {
-
-
+public class UserTitle extends Application {
+    @Override
+    public void start(Stage window){
+        Parameters param = getParameters();
+        String judul = param.getNamed().get("title");
+        
+        window.setTitle(judul);
+        window.show();
+    }
 }
